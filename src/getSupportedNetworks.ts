331e0a19cc2ece470b198c230edbe9a6ebb426e2
Denis Fadeev <denis@fadeev.org>
2023-07-11 16:22:37 +0400

    remove enum

--- src/getSupportedNetworks.ts
+++ src/getSupportedNetworks.ts
@@ -1,28 +1,19 @@
 import { networks } from "./networks";
 
 /**
- * Enum representing different types of network protocols.
- *
- * @member {string} CCM - Cross-Chain Messaging.
- */
-export enum NetworkType {
-  CCM = "ccm",
-}
-
-/**
  * Retrieves the list of supported networks. If a network type is provided, the
  * function returns networks that support the specific functionality. If no type
  * is provided, it returns all networks.
  *
  * @param {NetworkType} [type] - Optional argument to return only networks that
- * support specific functionality.
+ * support specific functionality. Accepts "ccm".
  *
  * @returns {string[]} An array of strings representing the supported networks.
  */
-export const getSupportedNetworks = (type?: NetworkType): string[] => {
+export const getSupportedNetworks = (type?: string): string[] => {
   let supportedNetworks = Object.keys(networks);
 
-  if (type === NetworkType.CCM) {
+  if (type === "ccm") {
     const excluded = [
       "btc_testnet",
       "zeta_testnet",
